1442923c4cc742c4f86f31a661115eaa681490e2
Bob DiMaiolo <bdimaiolo@gmail.com>
2024-07-05 11:49:32 -0400

    microbit cpp basic serial send

--- device-serial-microbit/microbit-v2-samples/source/main.cpp
+++ device-serial-microbit/microbit-v2-samples/source/main.cpp
@@ -3,12 +3,35 @@
 
 MicroBit uBit;
 
+void onButtonA(MicroBitEvent e)
+{
+    int x = uBit.accelerometer.getX();
+    int y = uBit.accelerometer.getY();
+    int z = uBit.accelerometer.getZ();
+    uBit.display.print("A");
+    uBit.serial.send("Button: A\r\n");
+    uBit.serial.printf("Accelerometer x:%d y:%d z:%d \r\n",x,y,z);
+}
+
+void onButtonB(MicroBitEvent e)
+{
+    int x = uBit.accelerometer.getX();
+    int y = uBit.accelerometer.getY();
+    int z = uBit.accelerometer.getZ();
+    uBit.display.print("B");
+    uBit.serial.send("Button: B\r\n");
+    uBit.serial.printf("Accelerometer x:%d y:%d z:%d \r\n",x,y,z);
+}
+
 int main()
 {
     uBit.init();
 
-    out_of_box_experience();
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_A, MICROBIT_BUTTON_EVT_CLICK, onButtonA);
+    uBit.messageBus.listen(MICROBIT_ID_BUTTON_B, MICROBIT_BUTTON_EVT_CLICK, onButtonB);
 
-    microbit_panic( 999 );
+    uBit.display.print("S");
+    uBit.serial.send("Starting Micro:Bit Program \r\n");
+
+    release_fiber();
 }
-
